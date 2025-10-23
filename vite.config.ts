ff6ba3bd893109eb67c5f9575db760265842ac28
Tudor Muntean <Tudor.Muntean@endava.com>
2025-10-23 12:09:20 +0300

    d

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 // #vite.config.ts
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/lawgentic-chatbot'
-})
+  base: "/lawgentic-chatbot",
+});
